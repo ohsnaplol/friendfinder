5867e3113508a1daeb037e132bffc851f9a8fd24
ohsnaplol <emyb@bellsouth.net>
2018-02-03 10:19:27 -0500

    fixed algorithm for finding friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -24,6 +24,7 @@ module.exports = function(app) {
       console.log(`Is ${difference} < ${bestFriendDifference}?`)
       if (difference < bestFriendDifference) {
         closestMatch = friends[i]
+        bestFriendDifference = difference
       }
     }
     console.log(closestMatch)
