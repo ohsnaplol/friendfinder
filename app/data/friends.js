cf3c4af8d3d6dcdc5bcfa3a60e75390e89d78ee0
ohsnaplol <emyb@bellsouth.net>
2018-02-03 10:19:02 -0500

    added celebrities (thanks michael castillo!)

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,32 @@
 var friends = [
   {
-    name: 'Ahmed',
+    name: "Daniela Ryf",
+    photo: "https://tri-mag.de/files/styles/teaser_image_small/public/media/video/file/c4ca4/10cce956921d3b2df0b61539a97f1bcc.jpg?itok=82m7hvHQ",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
-    name: 'Jacob Deming',
+    name: "Sebastian Kienle",
+    photo: "https://www.tritime-magazin.de/wp-content/uploads/2016/09/Sebastian-Kienle.jpg",
     scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
   },
   {
-    name: 'Jeremiah Scanlon',
+    name: "Neil Patrick",
+    photo: "https://pmcdeadline2.files.wordpress.com/2017/03/neil-patrick-harris-2.jpg?w=605",
     scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
   },
   {
-    name: 'Louis T. Delia',
+    name: "Jeff Dunham",
+    photo: "http://onwardstate.com/wp-content/uploads/2013/11/ccsu_dunham_controlledchaos_06.jpg",
     scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
   },
   {
-    name: 'Lou Ritter',
+    name: "Kristen Bell",
+    photo: "https://vignette.wikia.nocookie.net/simpsons/images/9/9f/Kristen_Bell.jpg/revision/latest?cb=20150715121846",
     scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
   },
   {
-    name: 'Jordan Biason',
+    name: "Emma Stone",
+    photo: "http://traceycunningham.com/wp-content/uploads/2015/12/landscape-1450730783-upload-2.png",
     scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
   }
 ]
