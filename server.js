34a17d9e2c2b5a5bb7663bd7e4055eb094ad46c1
ohsnaplol <emyb@bellsouth.net>
2018-02-03 13:46:39 -0500

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var apiRoutes = require('./app/routing/apiRoutes.js')
 // Sets up the Express App
 // =============================================================
 var app = express()
-const PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 3000
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
